325c489b0b4f5cf1f24171658b0310d1160c4eb7
pauloricardofagundes <paulo.fagundes@icloud.com>
2024-09-14 20:39:20 -0300

    nem file: Teste_MVP.py

---
+++ Teste_MVP.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/pauloricardofagundes/MVP/c85a753b69764764c7d605d8c61b3bd5f2eeade2/car_insurance_claim.csv',sep=',')
+resumo = df.head()
+print(resumo)
