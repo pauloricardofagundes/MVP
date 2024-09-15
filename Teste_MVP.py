e8db072f56f4a81b4d15c634ca0a8c225f0f6df4
pauloricardofagundes <paulo.fagundes@icloud.com>
2024-09-15 00:37:45 -0300

    new file: credit_card_transactions_4

--- Teste_MVP.py
+++ Teste_MVP.py
@@ -1,8 +1,18 @@
 import pandas as pd
 
+## Lista de datasets a serem utilizados para consolidar o arquivo final
+Databases = {'https://raw.githubusercontent.com/pauloricardofagundes/MVP/main/credit_card_transactions_1.csv',
+             'https://raw.githubusercontent.com/pauloricardofagundes/MVP/main/credit_card_transactions_2.csv',
+             'https://raw.githubusercontent.com/pauloricardofagundes/MVP/main/credit_card_transactions_3.csv'}
 
-Databases = {}
+## Leitura dos datasets da lista e consolida todos em um único dataset
 
-df = pd.read_csv('https://raw.githubusercontent.com/pauloricardofagundes/MVP/c85a753b69764764c7d605d8c61b3bd5f2eeade2/car_insurance_claim.csv',sep=',')
-resumo = df.head()
-print(resumo)
+df_list = []
+for filename in sorted(Databases):
+    df_list.append(pd.read_csv(filename, dtype='str',header=None).set_index(0))
+full_df = pd.concat(df_list)
+
+print(full_df.columns)
+full_df = full_df.rename(columns=full_df.iloc[0]).drop(full_df.index[0])
+print (full_df)
+full_df.to_csv(r'D:\Documentos\iCloud\iCloudDrive\Dev\teste.csv', sep='|', index=False)
--- from fsplit.py
+++
@@ -1,8 +0,0 @@
-import os
-from filesplit.split import Split
-
-LINES_PER_FILE = 93_000  # see PEP515 for readable numeric literals 
-filename = 'D:/Documentos/iCloud/iCloudDrive/Dev/credit_card_transactions.csv'
-outdir = 'D:/Documentos/iCloud/iCloudDrive/Dev'  # to store split-files `myinput_1.txt` etc.
-
-Split(filename, outdir).bylinecount(LINES_PER_FILE)
