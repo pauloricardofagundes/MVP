901dff53898ebc99c39352e7017a2fdef429b208
pauloricardofagundes <paulo.fagundes@icloud.com>
2024-09-14 22:37:20 -0300

    deleted: MVP

--- Teste_MVP.py
+++ Teste_MVP.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 
+Databases = {}
+
 df = pd.read_csv('https://raw.githubusercontent.com/pauloricardofagundes/MVP/c85a753b69764764c7d605d8c61b3bd5f2eeade2/car_insurance_claim.csv',sep=',')
 resumo = df.head()
 print(resumo)
